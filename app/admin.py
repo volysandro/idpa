f3fb7320b7f515b40d4b9eaf86895c29ee8dce41
Sandro Volery <sandro@volery.com>
2021-11-29 12:53:47 +0000

    add route to list manager accounts

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,20 @@
 from flask import Blueprint
 from . import db
+from .models import User
+import json
 
 admin_blueprint = Blueprint('admin', __name__)
 
 @admin_blueprint.route('/admin')
 def admin():
     return 'Admin'
+
+@admin_blueprint.route('/admin/api/list_manager')
+def list_manager():
+    managers = User.query.filter_by(manager=True).all()
+    list = []
+    for manager in managers:
+        list.append([manager.email, manager.name, manager.active, manager.reset_upon_login])
+    print(list)
+    return list
+    
