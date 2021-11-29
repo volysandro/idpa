b8d5bcdef37c73201ee96c5c9b58b2bfda2cf3db
Sandro Volery <sandro@volery.com>
2021-11-29 12:20:30 +0000

    working login / manager signup

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models import User
@@ -16,6 +16,8 @@ def signup():
 
 @auth_blueprint.route('/login', methods=['POST'])
 def login_post():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.profile'))
     email = request.form.get('email')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
@@ -28,16 +30,24 @@ def login_post():
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
+    if not user.active:
+        flash('This manager account is not active yet. Please notify a site administrator or wait until he enables the account.')
+        return redirect(url_for('auth.login'))
+
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
 
 @auth_blueprint.route('/signup', methods=['POST'])
 def signup_post():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.profile'))
+
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
-
+    manager = True if request.form.get('manager') else False
+    active = False if request.form.get('manager') else True
     user = User.query.filter_by(email=email).first()
 
     if user:
@@ -45,7 +55,7 @@ def signup_post():
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), manager=manager, active=active)
 
     # add the new user to the database
     db.session.add(new_user)
@@ -54,4 +64,5 @@ def signup_post():
 
 @auth_blueprint.route('/logout')
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('auth.login'))
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from . import db
+from flask_login import current_user
 
 main_blueprint = Blueprint('main', __name__)
 
@@ -9,4 +10,4 @@ def index():
 
 @main_blueprint.route('/profile')
 def profile():
-    return 'Profile'
+    return current_user.name + "   " + current_user.email
